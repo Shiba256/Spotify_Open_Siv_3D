1276c086d7f04bb0859cf555dd511fbaf2030008
Shiba256 <127299024+Shiba256@users.noreply.github.com>
2024-03-29 16:32:24 +0900

    Add HashTable to "TrackFeature"

--- TrackFeature.cpp
+++ TrackFeature.cpp
@@ -20,7 +20,40 @@ void Spotify::TrackFeature::set(const JSON& _json) {
 	tempo = _json[Type::tempo].getOr<double>(default_value);
 	time_signature = _json[Type::time_signature].getOr<double>(default_value);
 	valence = _json[Type::valence].getOr<double>(default_value);
-	duration_ms = static_cast<MicrosecondsF>(_json[Type::duration_ms].getOr<long double>(0.0));
+	duration_ms = static_cast<MillisecondsF>(_json[Type::duration_ms].getOr<long double>(0.0));
 	key = static_cast<Key>(_json[Type::key].getOr<int>(-1));
 	mode = static_cast<Mode>(_json[Type::mode].getOr<int>(1));
 }
+
+HashTable<String, double> Spotify::TrackFeature::AsHashTable() const noexcept {
+	return HashTable<String, double>{
+		{Type::acousticness, acousticness},
+		{ Type::danceability,danceability },
+		{ Type::energy,energy },
+		{ Type::instrumentalness,instrumentalness },
+		{ Type::liveness,liveness },
+		{ Type::loudness,loudness },
+		{ Type::speechiness,speechiness },
+		{ Type::tempo,tempo },
+		{ Type::time_signature,time_signature },
+		{ Type::valence,valence },
+		{Type::mode,static_cast<double>(mode)}
+	};
+}
+
+HashTable<String, double> Spotify::TrackFeature::AsHashTableNormalized(double tempo_max)const noexcept {
+	return HashTable<String, double>{
+		{Type::acousticness, acousticness},
+		{ Type::danceability,danceability },
+		{ Type::energy,energy },
+		{ Type::instrumentalness,instrumentalness },
+		{ Type::liveness,liveness },
+		{ Type::loudness,std::pow(2,loudness) },
+		{ Type::speechiness,speechiness },
+		{ Type::tempo,tempo/tempo_max },
+		{ Type::time_signature,(time_signature-3)/4 },
+		{ Type::valence,valence },
+		{ Type::mode,static_cast<double>(mode) }
+	};
+}
+
